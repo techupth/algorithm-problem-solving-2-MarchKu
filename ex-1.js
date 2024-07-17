576697b1994cde0745a8d216558fdd45b25da9b1
Kunakorn <kunakorn.ph98@gmail.com>
2024-07-17 14:50:37 +0700

    March

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,10 @@
 function findStudentIndex(students, searchStudent) {
 	// เริ่มเขียนโค้ดตรงนี้จ้า
+	return students.indexOf(searchStudent)
+
+
 }
 
 // ตอบคำถามตรงนี้จ้า
+console.log(findStudentIndex(["Andrew", "Bobby", "Diana", "George", "Hannah", "Isaac", "Jasmine", "John"],"John") )
+// ans 7
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,28 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  for(let i in books){
+    console.log(books[i].title)
+
+    if(books[i].title === searchTitle){
+      return i
+    }
+    
+
 }
+return -1}
+
+booksArr = [
+    { title: "A Tale of Two Cities", author: "Charles Dickens" },
+    { title: "Brave New World", author: "Aldous Huxley" },
+    { title: "Catch-22", author: "Joseph Heller" },
+    { title: "Don Quixote", author: "Miguel de Cervantes" },
+    { title: "Ender's Game", author: "Orson Scott Card" },
+    { title: "Fahrenheit 451", author: "Ray Bradbury" },
+    { title: "Gone with the Wind", author: "Margaret Mitchell" },
+    { title: "Harry Potter", author: "J.K. Rowling" }
+];
+searchBook1 = "Gone with the Wind"
+searchBook2 = "The Master Mind"
+
+console.log(findBookIndex(booksArr,searchBook1)) // 6
+console.log(findBookIndex(booksArr,searchBook2)) // -1
